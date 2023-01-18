e4862e2670f315a8704a5a611bcbc54aee4374d3
Daryl <dyk0203@naver.com>
2023-01-19 01:59:17 +0900

    9.1 Accounts Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,7 +3,17 @@ import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: { ...payload },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
   return res.status(200).end();
 }
 
